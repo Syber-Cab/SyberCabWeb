ac163583e9d8c2a25cefc2bf4abc1a0c34bbfc3a
sybercab <Carolyn.Pesola89079@mail.ru>
2022-04-07 19:22:56 +0800

    deploy web : V0.01:init

---
+++ assets/welcome.html.7013b27e.js
@@ -0,0 +1,15 @@
+const e={key:"v-1abeb830",path:"/welcome.html",title:"Welcome to Syber.Cab",lang:"en-US",frontmatter:{index:1,title:"Welcome to Syber.Cab",author:"Syber.Cab",date:"2022-03-28T00:00:00.000Z",category:["Guide"],tag:["Welcome"],sticky:!0,star:!0,summary:`Syber Cab : Syber Meta cab.
+You can support us at:
+
+gitcoin.co :
+dorahacks.io :
+devpost.com :
+
+`,head:[["meta",{property:"og:url",content:"https://syber.cab/welcome.html"}],["meta",{property:"og:title",content:"Welcome to Syber.Cab"}],["meta",{property:"og:type",content:"article"}],["meta",{property:"og:locale",content:"en-US"}],["meta",{property:"og:locale:alternate",content:"zh-CN"}],["meta",{property:"article:author",content:"Syber.Cab"}],["meta",{property:"article:tag",content:"Welcome"}],["meta",{property:"article:published_time",content:"2022-03-28T00:00:00.000Z"}]]},excerpt:`<p>Syber Cab : Syber Meta cab.</p>
+<p>You can support us at:</p>
+<ol>
+<li>gitcoin.co :</li>
+<li>dorahacks.io :</li>
+<li>devpost.com :</li>
+</ol>
+`,headers:[{level:2,title:"Syber Cab",slug:"syber-cab",children:[]},{level:2,title:"Syber Connect",slug:"syber-connect",children:[]},{level:2,title:"Why SyberCab?",slug:"why-sybercab",children:[]},{level:2,title:"Tech Stack to Build Syber Cab",slug:"tech-stack-to-build-syber-cab",children:[{level:3,title:"Identity Management",slug:"identity-management",children:[]},{level:3,title:"Account System",slug:"account-system",children:[]},{level:3,title:"Decentralized Networks",slug:"decentralized-networks",children:[]}]}],git:{createdTime:1649330545e3,updatedTime:1649330545e3,contributors:[{name:"sybercab",email:"Carolyn.Pesola89079@mail.ru",commits:1}]},readingTime:{minutes:.44,words:133},filePathRelative:"welcome.md"};export{e as data};
